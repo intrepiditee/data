41920ea2c6c96d835171df42bf98853e5adea95e
intrepiditee <31870027+intrepiditee@users.noreply.github.com>
2020-07-20 11:59:33 -0500

    remove files

--- import-automation/executor/test/test_utils.py
+++ import-automation/executor/test/test_utils.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Tests for app/utils.py and test/utils.py.
+"""
+
 import unittest
 from unittest import mock
 import datetime
